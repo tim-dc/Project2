adf43bd6cd80d76b193237480fa99f92a6d9cd5f
tim-dc <77545252+tim-dc@users.noreply.github.com>
2022-06-12 19:21:15 +0800

    Added randomized passenger wait times. Added Queue for passengers waiting to be seated.

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Driver {
+
+	static Passenger passengers[];
+	static Queue<Passenger> line = new LinkedList<>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -9,32 +14,52 @@ public class Driver {
 		int n, C, m;
 		Scanner scan = new Scanner(System.in);
 		ArrayList<Car> cars = new ArrayList<Car>();
+
 		
 		ArrayList<Semaphore> LoadingArea = new ArrayList<Semaphore>();
 		ArrayList<Semaphore> UnLoadingArea = new ArrayList<Semaphore>();
 		
 		System.out.print("Enter the number of passenger processes: ");
 		n = scan.nextInt();
-		
-		System.out.print("Enter the capacity of the car: ");
-		C = scan.nextInt();
-		
-		System.out.print("Enter the number of cars: ");
-		m = scan.nextInt();
-		
-		System.out.printf("Enter n: %d " + "Enter C: %d " + "Enter m: %d ", n, C, m);
-		
-		for(int i = 0; i < m - 1; i++) {
-			cars.add(new Car(C, i));
+
+
+		// Instantiate Passengers
+		passengers = new Passenger[n];
+
+		for(int i = 0; i < n; i++){
+			passengers[i] = new Passenger(i);
 		}
-		
-		for(int j = 0; j < m; j++) {
-			LoadingArea.add(new Semaphore());
+
+
+		for(int i = 0; i < n; i++){
+			System.out.println("Passenger #" + passengers[i].getPassengerID());
+			passengers[i].start();
 		}
+
+		System.out.println("--QUEUE--");
+
+
+
 		
-		for(int k = 0; k < m; k++) {
-			UnLoadingArea.add(new Semaphore());
-		}
+//		System.out.print("Enter the capacity of the car: ");
+//		C = scan.nextInt();
+//
+//		System.out.print("Enter the number of cars: ");
+//		m = scan.nextInt();
+//
+//		System.out.printf("Enter n: %d " + "Enter C: %d " + "Enter m: %d ", n, C, m);
+		
+//		for(int i = 0; i < m - 1; i++) {
+//			cars.add(new Car(C, i));
+//		}
+//
+//		for(int j = 0; j < m; j++) {
+//			LoadingArea.add(new Semaphore());
+//		}
+//
+//		for(int k = 0; k < m; k++) {
+//			UnLoadingArea.add(new Semaphore());
+//		}
 
 	}
 
--- src/Passenger.java
+++ src/Passenger.java
@@ -1,8 +1,9 @@
-public class Passenger {
+public class Passenger extends Thread{
 
+    int passengerID, carID;
 
-    public Passenger() {
-
+    Passenger(int passengerID) {
+        this.passengerID = passengerID;
     }
 
     public void board(int carID) {
@@ -11,5 +12,48 @@ public class Passenger {
 
     public void unboard(int carID) {
 
+    }
+
+    private void wander() {
+        try{
+            Thread.sleep(Math.round(Math.random() * 1000));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+
+        // Wait (wander)
+//        wander();
+
+        long a = Math.round(Math.random());
+        if(a%2 == 0){
+            System.out.println(a);
+            Driver.line.add(this);
+        }else{
+            try{
+                Thread.sleep(Math.round(Math.random()) * 1000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            // Get in Line
+            Driver.line.add(this);
+        }
+
+        for(int i =0; i < Driver.line.size();i++){
+            // This would be replaced by cars
+            System.out.println("Queue " + Driver.line.remove());
+        }
+    }
+
+
+
+    public int getPassengerID() {
+        return passengerID;
+    }
+
+    public int getCarID() {
+        return carID;
     }
 }
